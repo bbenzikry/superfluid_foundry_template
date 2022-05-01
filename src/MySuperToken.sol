8cff43dd6ed7e83c4299c1a4b3700196d11ed30d
Joshua Trujillo <Jtriley15@gmail.com>
2022-05-01 08:51:38 -0400

    updated for superfluid contracts ^0.2.x

--- src/MySuperToken.sol
+++ src/MySuperToken.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.0;
 
 import {ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperToken.sol";
-import {NativeSuperTokenProxy} from "@superfluid-finance/ethereum-contracts/contracts/tokens/NativeSuperToken.sol";
+import {PureSuperToken} from "@superfluid-finance/ethereum-contracts/contracts/tokens/PureSuperToken.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Example Super Token
 /// @author jtriley.eth
 /// @notice For demonstration only. You can delete this file.
-contract MySuperToken is NativeSuperTokenProxy, Ownable {
+contract MySuperToken is PureSuperToken, Ownable {
     /// @notice Mints to any receiver
     /// @param receiver Receiving address of mint
     /// @param amount Amount to mint
